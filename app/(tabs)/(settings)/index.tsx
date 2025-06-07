14ae61667a164ba5c69c174e58f446629a99f78f
baoha7204 <baoha3604@gmail.com>
2025-06-07 09:39:16 +0700

    feat: add Sign out button

--- app/(tabs)/(settings)/index.tsx
+++ app/(tabs)/(settings)/index.tsx
@@ -1,7 +1,25 @@
 import { Text } from "@/components/ui/text";
+import * as Linking from "expo-linking";
+import { useClerk } from "@clerk/clerk-expo";
+import { TouchableOpacity } from "react-native";
 
 const SettingsScreen = () => {
-  return <Text>Settings screen</Text>;
+  const { signOut } = useClerk();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      Linking.openURL(Linking.createURL("/sign-in"));
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text>Sign out</Text>
+    </TouchableOpacity>
+  );
 };
 
 export default SettingsScreen;
